76a5e71c1b86403fa6fe8d9ff73e2314de34e2cc
pylin-club <happytrialtester@gmail.com>
2024-05-29 01:54:50 +0330

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -203,16 +203,16 @@ endipv4(){
 
 
 endipresult(){
-	temp_var=$1
+	num_configs=$1
 	echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u > ip.txt
 	ulimit -n 102400
 	chmod +x warpendpoint
 	./warpendpoint
-	clear
+	echo "------------------------------------------------------------"
 	echo "${GREEN}successfully generated ipv4 endip list${RESET}"
 	echo "${GREEN}successfully create result.csv file${RESET}"
 	echo "${CYAN}Now we're going to process result.csv${RESET}"
-	process_result_csv $temp_var
+	process_result_csv $num_configs
 	rm -rf ip.txt warpendpoint result.csv
 	exit
 }
@@ -223,28 +223,27 @@ get_values() {
     local private_key=$(echo "$api_output" | grep -oE '"private_key":"[0-9a-zA-Z\/+]+=+"' | sed 's/"private_key":"//; s/"//')
     local public_key=$(echo "$api_output" | grep -oE '"peer_public_key":"[0-9a-zA-Z\/+]+=+"' | sed 's/"peer_public_key":"//; s/"//')
     local reserved=$(echo "$api_output" | grep -oE '"reserved":\[[0-9]+(,[0-9]+){2}\]' | sed 's/"reserved"://; s/\[//; s/\]//')
-    # echo "$ipv6@$private_key@$public_key@$reserved"
 }
 
 process_result_csv() {
 count_conf=$1
 
     # تعداد سطرهای فایل result.csv را بدست آورید
-    num_lines=100 #$(wc -l < ./result.csv)
-    echo ""
+    num_lines=$(wc -l < ./result.csv)
+    echo "------------------------------------------------------------"
     echo "We have considered the number of ${num_lines} IPs."
     echo ""
 
-if [ "$count_conf" -lt "$num_lines" ]; then
-    num_lines=$count_conf
-elif [ "$count_conf" -gt "$num_lines" ]; then
-    echo "Warning: The number of IPs found is less than the number you want!"
-    num_lines=$count_conf
-fi
+	if [ "$count_conf" -lt "$num_lines" ]; then
+	    num_lines=$count_conf
+	elif [ "$count_conf" -gt "$num_lines" ]; then
+	    echo "Warning: The number of IPs found is less than the number you want!"
+	    num_lines=$count_conf
+	fi
 
     # برای هر سطر در result.csv دستورات را اجرا کنید
     for ((i=2; i<=$num_lines; i++)); do
-    	echo "num: ${i-1}, done."
+    	echo "license $((i-1)), captured."
         # اطلاعات مورد نیاز از هر سطر را دریافت کنید
         local line=$(sed -n "${i}p" ./result.csv)
         local endpoint=$(echo "$line" | awk -F',' '{print $1}')
@@ -263,68 +262,161 @@ fi
         i_w_pb=$(echo "$i_values" | cut -d'@' -f3)
         i_w_res=$(echo "$i_values" | cut -d'@' -f4)
 
-# ترکیب رشته و متغیرها در یک متغیر دیگر
-new_json='{
-      "type": "wireguard",
-      "tag": "Warp-IR'"${i-1}"'",
-      "server": "engage.cloudflareclient.com",
-      "server_port": 2408,
+		# ترکیب رشته و متغیرها در یک متغیر دیگر
 
-      "local_address": [
-        "172.16.0.2/32",
-        "'"$w_ip"'"
-      ],
-      "private_key": "'"$w_pv"'",
-      "peer_public_key": "'"$w_pb"'",
-      "reserved": ['$w_res'],
-
-      "mtu": 1280,
-      "fake_packets": "5-10"
-    },
-    {
-      "type": "wireguard",
-      "tag": "Warp-Main'"${i-1}"'",
-      "detour": "Warp-IR'"${i-1}"'",
-      "server": "engage.cloudflareclient.com",
-      "server_port": 2408,
-      
-      "local_address": [
-          "172.16.0.2/32",
-          "'"$i_w_ip"'"
-      ],
-      "private_key": "'"$i_w_pv"'",
-      "peer_public_key": "'"$i_w_pb"'",
-      "reserved": ['$i_w_res'],  
-
-      "mtu": 1120
-    }'
-
-
-    temp_json+="$new_json"
-    # اضافه کردن خط خالی به محتوای متغیر موقت
-if [ $i -lt $num_lines ]; then
-    temp_json+=","
-fi
-
+  		if [ $((i % 2)) -eq 0 ]; then
+		    value_to_add="@pylin_news"
+		else
+		    value_to_add="@pylin_gap"
+		fi
+  		
+		new_json='{
+	      "type": "wireguard",
+	      "tag": "\ud83c\udf10Web_'"${i-1}"' | $value_to_add",
+	      "server": "'"$ip"'",
+	      "server_port": '"$port"',
+	
+	      "local_address": [
+	        "172.16.0.2/32",
+	        "'"$w_ip"'"
+	      ],
+	      "private_key": "'"$w_pv"'",
+	      "peer_public_key": "'"$w_pb"'",
+	      "reserved": ['$w_res'],
+	
+	      "mtu": 1280,
+	      "fake_packets": "1-6"
+	    },
+	    {
+	      "type": "wireguard",
+	      "tag": "\ud83c\udfaeGame_'"${i-1}"' | $value_to_add",
+	      "detour": "\ud83c\udf10Web_'"${i-1}"' | $value_to_add",
+	      "server": "'"$ip"'",
+	      "server_port": '"$port"',
+	      
+	      "local_address": [
+	          "172.16.0.2/32",
+	          "'"$i_w_ip"'"
+	      ],
+	      "private_key": "'"$i_w_pv"'",
+	      "peer_public_key": "'"$i_w_pb"'",
+	      "reserved": ['$i_w_res'],  
+	
+	      "mtu": 1280,
+	      "fake_packets": "1-6"
+	    }'
+	
+	    temp_json+="$new_json"
+	
+		# اضافه کردن خط خالی به محتوای متغیر موقت
+		if [ $i -lt $num_lines ]; then
+		    temp_json+=","
+		fi
     done
-full_json='
-{
-  "outbounds": 
-  [
-    '"$temp_json"'
-  ]
+	
+	full_json='
+	{
+	  "outbounds": 
+	  [
+	    '"$temp_json"'
+	  ]
+	}
+	'
+	echo "$full_json" > warp.json
+	echo ""
+	echo "${GREEN}Upload Files to Get Link${RESET}"
+	echo "------------------------------------------------------------"
+	echo "Your link:"
+	curl https://bashupload.com/ -T warp.json | sed -e 's#wget#Your Link#' -e 's#https://bashupload.com/\(.*\)#https://bashupload.com/\1?download=1#'
+	echo "------------------------------------------------------------"
+	echo ""
+	mv warp.json warp_$(date +"%Y%m%d_%H%M%S").json
 }
-'
-echo "$full_json" > output.json
-echo ""
-echo "${GREEN}Upload Files to Get Link${RESET}"
-echo "------------------------------------------------------------"
-echo ""
-echo "Your link:"
-curl https://bashupload.com/ -T output.json | sed -e 's#wget#Your Link#' -e 's#https://bashupload.com/\(.*\)#https://bashupload.com/\1?download=1#'
-echo "------------------------------------------------------------"
-echo ""
-mv output.json output_$(date +"%Y%m%d_%H%M%S").json
+
+process_no_result_csv() {
+count_conf=$1
+    for ((i=2; i<=$num_lines; i++)); do
+		echo "license $((i-1)), captured."
+  
+        values=$(get_values)
+        w_ip=$(echo "$values" | cut -d'@' -f1)
+        w_pv=$(echo "$values" | cut -d'@' -f2)
+        w_pb=$(echo "$values" | cut -d'@' -f3)
+        w_res=$(echo "$values" | cut -d'@' -f4)
+        
+        i_values=$(get_values)
+        i_w_ip=$(echo "$i_values" | cut -d'@' -f1)
+        i_w_pv=$(echo "$i_values" | cut -d'@' -f2)
+        i_w_pb=$(echo "$i_values" | cut -d'@' -f3)
+        i_w_res=$(echo "$i_values" | cut -d'@' -f4)
+
+		if [ $((i % 2)) -eq 0 ]; then
+		    value_to_add="@pylin_news"
+		else
+		    value_to_add="@pylin_gap"
+		fi
+  
+		new_json='{
+	      "type": "wireguard",
+	      "tag": "Warp-IR'"${i-1}"' | $value_to_add",
+	      "server": "engage.cloudflareclient.com",
+	      "server_port": 2408,
+	
+	      "local_address": [
+	        "172.16.0.2/32",
+	        "'"$w_ip"'"
+	      ],
+	      "private_key": "'"$w_pv"'",
+	      "peer_public_key": "'"$w_pb"'",
+	      "reserved": ['$w_res'],
+	
+	      "mtu": 1280,
+	      "fake_packets": "1-6"
+	    },
+	    {
+	      "type": "wireguard",
+	      "tag": "Warp-Main'"${i-1}"' | $value_to_add",
+	      "detour": "Warp-IR'"${i-1}"' | $value_to_add",
+	      "server": "engage.cloudflareclient.com",
+	      "server_port": 2408,
+	      
+	      "local_address": [
+	          "172.16.0.2/32",
+	          "'"$i_w_ip"'"
+	      ],
+	      "private_key": "'"$i_w_pv"'",
+	      "peer_public_key": "'"$i_w_pb"'",
+	      "reserved": ['$i_w_res'],  
+	
+	      "mtu": 1280,
+	      "fake_packets": "1-6"
+	    }'
+	
+	    temp_json+="$new_json"
+	
+		# اضافه کردن خط خالی به محتوای متغیر موقت
+		if [ $i -lt $num_lines ]; then
+		    temp_json+=","
+		fi
+	done
+		
+	full_json='
+	{
+	  "outbounds": 
+	  [
+		'"$temp_json"'
+	  ]
+	}
+	'
+	echo "$full_json" > warp.json
+	echo ""
+	echo "${GREEN}Upload Files to Get Link${RESET}"
+	echo "------------------------------------------------------------"
+	echo "Your link:"
+	curl https://bashupload.com/ -T warp.json | sed -e 's#wget#Your Link#' -e 's#https://bashupload.com/\(.*\)#https://bashupload.com/\1?download=1#'
+	echo "------------------------------------------------------------"
+	echo ""
+	mv warp.json warp_$(date +"%Y%m%d_%H%M%S").json
 }
 
 menu(){
@@ -340,11 +432,16 @@ menu(){
 	read -r -p "Please choose an option: " option
 
 	if [ "$option" = "1" ]; then
- 		read -r -p "Number of IPs to scan (e.g. 100): " number_of_ips
+ 		read -r -p "Number of IPs to scan (e.g. 100) [0 for engage.cloudflareclient.com:2408]: " number_of_ips
 		read -r -p "Number of configurations (e.g. 10): " number_of_configs
-		cfwarpIP
-		endipv4 $number_of_ips
-		endipresult $number_of_configs
+
+    	if [ "$number_of_ips" -eq 0 ]; then
+			cfwarpIP
+			endipv4 $number_of_ips
+			endipresult $number_of_configs
+   		else
+     		process_result_csv $number_of_configs
+   		fi
 	elif [ "$option" = "2" ]; then
 		read -r -p "Number of configurations(e.g. 10): " number_of_configs
 		process_result_csv $number_of_configs
