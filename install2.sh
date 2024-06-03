8ee10021f1a18c45f99a9b6f507200b82a98536a
pylin-club <happytrialtester@gmail.com>
2024-06-03 23:27:30 +0330

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GREEN=$(tput setaf 2)
+GREEN= $(tput setaf 2)
 YELLOW=$(tput setaf 3)
 BLUE=$(tput setaf 4)
 CYAN=$(tput setaf 6)
@@ -26,6 +26,86 @@ case "$(uname -m)" in
 	;;
 esac
 
+subnets=(
+	# Original
+	"162.159.192."
+	# "162.159.193."
+	# "162.159.195."
+	"188.114.96."
+	"188.114.97."
+	# "188.114.98."
+	"188.114.99."
+	
+	# Germany -> 172.64.0.0/13
+	# "172.64.33."
+	"172.64.97."
+	"172.64.161."
+	"172.64.225."
+	
+	"172.65.33."
+	"172.65.97."
+	# "172.65.161."
+	"172.65.225."
+	
+	"172.67.33."
+	"172.67.97."
+	"172.67.161."
+	"172.67.225."
+	
+	# "172.69.33."
+	# "172.69.97."
+	"172.69.161."
+	# "172.69.225."
+	
+	# "172.71.33."
+	# "172.71.97."
+	# "172.71.161."
+	# "172.71.225."
+
+	# Netherlands -> 141.101.64.0/18
+	"141.101.65."
+	"141.101.67."
+	"141.101.75."
+	"141.101.82."
+
+	"141.101.97."
+	"141.101.98."
+	"141.101.107."
+	"141.101.108."
+
+	"141.101.116."
+	"141.101.117."
+	"141.101.126."
+	"141.101.127."
+
+
+	# temp  
+	# "8.6.144."
+	# "8.9.231."
+	# "8.10.148."
+	# "8.21.239."
+	# "8.45.102."
+	# "104.19.237."
+	# "108.162.236."
+	# "141.101.73."
+	"162.158.111."
+	# "162.158.173."
+	# "162.159.250."
+	# "172.64.203."
+	# "173.245.63."
+	# "188.114.99."
+	# "198.41.205."
+	# "199.27.132."
+)
+
+total_ips=$(( ${#subnets[@]} * 256 ))
+min_possible_ips=100
+max_possible_ips=$((total_ips / 8))
+
+min_configs=2
+max_configs=50
+
+
 cfwarpIP(){
 	echo "download warp endpoint file base on your CPU architecture"
 	if [[ -n $cpu ]]; then
@@ -34,84 +114,18 @@ cfwarpIP(){
 }
 
 endipv4(){
-	iplist=$1
- 
-	subnets=(
- 		# Original
-		"162.159.192."
-		# "162.159.193."
-		# "162.159.195."
-		"188.114.96."
-		"188.114.97."
-		# "188.114.98."
-		"188.114.99."
-  		
-		# Germany -> 172.64.0.0/13
-		# "172.64.33."
-		"172.64.97."
-		"172.64.161."
-		"172.64.225."
-		
-		"172.65.33."
-		"172.65.97."
-		# "172.65.161."
-		"172.65.225."
-		
-		"172.67.33."
-		"172.67.97."
-		"172.67.161."
-		"172.67.225."
-		
-		# "172.69.33."
-		# "172.69.97."
-		"172.69.161."
-		# "172.69.225."
-		
-		# "172.71.33."
-		# "172.71.97."
-		# "172.71.161."
-		# "172.71.225."
+	num_ips=$1
 
-		# Netherlands -> 141.101.64.0/18
-  		"141.101.65."
-		"141.101.67."
-		"141.101.75."
-		"141.101.82."
-  
-		"141.101.97."
-		"141.101.98."
-		"141.101.107."
-		"141.101.108."
-  
-		"141.101.116."
-		"141.101.117."
-		"141.101.126."
-		"141.101.127."
-
-
-		# temp  
-		# "8.6.144."
-		# "8.9.231."
-		# "8.10.148."
-		# "8.21.239."
-		# "8.45.102."
-		# "104.19.237."
-		# "108.162.236."
-		# "141.101.73."
-  		"162.158.111."
-		# "162.158.173."
-		# "162.159.250."
-		# "172.64.203."
-		# "173.245.63."
-		# "188.114.99."
-		# "198.41.205."
-		# "199.27.132."
-    
-    )
+	# check the range of IPs
+	if [[ ${num_ips} -lt ${min_possible_ips} ]]; then
+		num_ips=${min_possible_ips}
+	elif [[ ${num_ips} -gt ${max_possible_ips} ]]; then
+		num_ips=${max_possible_ips}
+	fi
 
 	# Initialize an empty array for responsive IPs
- 	# echo "------------------------------------------------------------"
-  # 	echo "ping ip pools (this might take some time!):"
+	# echo "------------------------------------------------------------"
+	# 	echo "ping ip pools (this might take some time!):"
 	# responsive_ips=()
 	
 	# Check ping for each IP
@@ -126,16 +140,16 @@ endipv4(){
 	#     fi
 	# done
 
-	#  	echo "Done! now let's scan $iplist ips"
+	#  	echo "Done! now let's scan $num_ips ips"
 	#    	echo "------------------------------------------------------------"
     
 	declare -ag distinct_ips
 
-	while [[ ${#distinct_ips[@]} -lt $iplist ]]; do
+	while [[ ${#distinct_ips[@]} -lt $num_ips ]]; do
 
 		for ip in "${subnets[@]}"; do
 
-			if [[ ${#distinct_ips[@]} == $iplist ]]; then
+			if [[ ${#distinct_ips[@]} == $num_ips ]]; then
 				break
 			fi
 			
@@ -167,7 +181,9 @@ endipresult(){
 	echo "${GREEN}successfully generated ipv4 endip list${RESET}"
 	echo "${GREEN}successfully create result.csv file${RESET}"
 	echo "${CYAN}Now we're going to process result.csv${RESET}"
+
 	process_result_csv $num_configs
+
 	rm -rf ip.txt warpendpoint result.csv
 	exit
 }
@@ -182,19 +198,14 @@ get_values() {
 }
 
 process_result_csv() {
-count_conf=$1
+	num_configs=$1
+	num_lines=$(wc -l < ./result.csv)
 
-    # تعداد سطرهای فایل result.csv را بدست آورید
-    num_lines=$(wc -l < ./result.csv)
-    echo "------------------------------------------------------------"
-    echo "We have considered the number of ${num_lines} IPs."
-    echo ""
-
-	if [ "$count_conf" -lt "$num_lines" ]; then
-		num_lines=$count_conf
-	elif [ "$count_conf" -gt "$num_lines" ]; then
-		echo "Warning: The number of IPs found is less than the number you want!"
-		num_lines=$count_conf
+	# check the range of IPs
+	if [[ ${num_configs} -lt ${min_configs} ]]; then
+		num_configs=${min_configs}
+	elif [[ ${num_configs} -gt ${max_configs} ]]; then
+		num_configs=${max_configs}
 	fi
 
     # loop over result.csv IPs
@@ -277,6 +288,16 @@ count_conf=$1
 			fi
 		fi
 	done
+
+
+    echo "------------------------------------------------------------"
+    echo "We have considered the number of ${counter} clean IPs."
+    echo ""
+	if [ "$counter" -gt "$num_configs" ]; then
+		echo "Warning: you have requested ${num_configs} configs but only ${counter} configs found."
+		echo "try using a bigger number for scanning IPs"
+	fi
+
 		
 	full_json='
 	{
@@ -297,89 +318,7 @@ count_conf=$1
 }
 
 process_no_result_csv() {
-	count_conf=$1
-	for ((i=2; i<=$count_conf; i++)); do
-		echo "license $((i-1)), captured."
-  
-        values=$(get_values)
-        w_ip=$(echo "$values" | cut -d'@' -f1)
-        w_pv=$(echo "$values" | cut -d'@' -f2)
-        w_pb=$(echo "$values" | cut -d'@' -f3)
-        w_res=$(echo "$values" | cut -d'@' -f4)
-        
-        i_values=$(get_values)
-        i_w_ip=$(echo "$i_values" | cut -d'@' -f1)
-        i_w_pv=$(echo "$i_values" | cut -d'@' -f2)
-        i_w_pb=$(echo "$i_values" | cut -d'@' -f3)
-        i_w_res=$(echo "$i_values" | cut -d'@' -f4)
-
-		if [ $((i % 2)) -eq 0 ]; then
-		    value_to_add="@pylin_news"
-		else
-		    value_to_add="@pylin_gap"
-		fi
-  
-		new_json='{
-	      "type": "wireguard",
-	      "tag": "\ud83c\udf10Web_'$((i - 1))' | '$value_to_add'",
-	      "server": "engage.cloudflareclient.com",
-	      "server_port": 2408,
-	
-	      "local_address": [
-	        "172.16.0.2/32",
-	        "'"$w_ip"'"
-	      ],
-	      "private_key": "'"$w_pv"'",
-	      "peer_public_key": "'"$w_pb"'",
-	      "reserved": ['$w_res'],
-	
-	      "mtu": 1280,
-	      "fake_packets": "5-10"
-	    },
-	    {
-	      "type": "wireguard",
-	      "tag": "\ud83c\udfaeGame_'$((i - 1))' | '$value_to_add'",
-	      "detour": "\ud83c\udf10Web_'$((i - 1))' | '$value_to_add'",
-	      "server": "engage.cloudflareclient.com",
-	      "server_port": 2408,
-	      
-	      "local_address": [
-	          "172.16.0.2/32",
-	          "'"$i_w_ip"'"
-	      ],
-	      "private_key": "'"$i_w_pv"'",
-	      "peer_public_key": "'"$i_w_pb"'",
-	      "reserved": ['$i_w_res'],  
-	
-	      "mtu": 1120,
-	      "fake_packets": "5-10"
-	    }'
-	
-	    temp_json+="$new_json"
-	
-		# اضافه کردن خط خالی به محتوای متغیر موقت
-		if [ $i -lt $count_conf ]; then
-		    temp_json+=","
-		fi
-	done
-		
-	full_json='
-	{
-	  "outbounds": 
-	  [
-		'"$temp_json"'
-	  ]
-	}
-	'
-	echo "$full_json" > warp.json
-	echo ""
-	echo "${GREEN}Upload Files to Get Link${RESET}"
-	echo "------------------------------------------------------------"
-	echo "Your link:"
-	curl https://bashupload.com/ -T warp.json | sed -e 's#wget#Your Link#' -e 's#https://bashupload.com/\(.*\)#https://bashupload.com/\1#'
-	echo "------------------------------------------------------------"
-	echo ""
-	mv warp.json warp_$(date +"%Y%m%d_%H%M%S").json
+	a=10
 }
 
 menu(){
@@ -392,14 +331,14 @@ menu(){
 	echo "------------------------------------------------------------"
 	echo "1.Automatic scanning and execution (Android / Linux)"
 	echo "2.Import custom IPs with result.csv file (windows)"
- 	echo ""
+	echo ""
 	read -r -p "Please choose an option: " option
 
 	if [ "$option" = "1" ]; then
- 		read -r -p "Number of IPs to scan (e.g. 100) [0 for engage.cloudflareclient.com:2408]: " number_of_ips
-		read -r -p "Number of configurations (e.g. 10): " number_of_configs
+		read -rep $"Number of IPs to scan (min:${min_possible_ips}, max:${max_possible_ips}) use 0 for engage.cloudflareclient.com:2408): " number_of_ips
+		read -rep $"Number of configurations (min:${min_configs}, max:${max_configs}) [5 is recommended] : " number_of_configs
 
-    	if [ "$number_of_ips" -ne 0 ]; then
+		if [ "$number_of_ips" -ne 0 ]; then
 			cfwarpIP
 			endipv4 $number_of_ips
 			endipresult $number_of_configs
@@ -407,7 +346,7 @@ menu(){
 			process_no_result_csv $number_of_configs
 		fi
 	elif [ "$option" = "2" ]; then
-		read -r -p "Number of configurations(e.g. 10): " number_of_configs
+		read -rep $"Number of configurations(e.g. 10): " number_of_configs
 		process_result_csv $number_of_configs
 	else
 		echo "Invalid option"
